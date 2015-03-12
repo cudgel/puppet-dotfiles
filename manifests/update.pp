c4d0ee27bad247924d8c471692520da4dbe84f42
Christopher Caldwell <caldwell@gwu.edu>
2015-03-12 08:47:46 -0400

    add path

--- manifests/update.pp
+++ manifests/update.pp
@@ -9,10 +9,11 @@ define dotfiles::update(
 
   if (!$single_pull) {
     exec { "update ${gituser} dotfiles for ${title}":
-      cwd     => "${cwd}",
+      cwd     => ${cwd},
+      path    => '/usr/bin',
       command => $rebase ? {true  => "git pull --rebase", false => "git pull", },
       onlyif  => "[ ! -f .git/FETCH_HEAD ] || [ \"$(( ( $(date +%s) - $(stat -c \"%Y\" .git/FETCH_HEAD) ) / 60 ))\" -ge \"${frequency}\" ]",
-      user    => "${title}",
+      user    => ${title},
       require => Package['git'],
     }
   }
