c6948d1a55d10741de8cb231243f0a635b1c280f
Christopher Caldwell <caldwell@gwu.edu>
2015-03-12 08:44:33 -0400

    add path, puppet style compliance

--- manifests/clone.pp
+++ manifests/clone.pp
@@ -1,16 +1,17 @@
 define dotfiles::clone(
     $gituser,
-    $giturl  = "git://github.com",
-    $project = "dotfiles",
-    $branch  = "master",
     $homedir,
     $creates,
+    $giturl  = 'git://github.com',
+    $project = 'dotfiles',
+    $branch  = 'master'
   ) {
   exec { "clone ${gituser} dotfiles for ${title}":
-    cwd     => "${homedir}",
-    command => "git clone ${giturl}/${gituser}/${project}.git --branch $branch",
-    creates => "${creates}",
-    user    => "${title}",
+    cwd     => $homedir,
+    command => "git clone ${giturl}/${gituser}/${project}.git --branch ${branch}",
+    path    => '/usr/bin',
+    creates => $creates,
+    user    => $title,
     require => Package['git'],
   }
 }
